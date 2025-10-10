64e860ff6677cfcd697e4c78d63b84422e00bd2e
JerryShum <jerryshum2636@gmail.com>
2025-10-10 11:45:36 -0400

    install lucide-react icons

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Navbar from "../components/custom/navbar";
 
 export const Route = createRootRoute({
-	component: () => (
-		<>
-			<Outlet />
-			<TanStackRouterDevtools />
-		</>
-	),
+  component: () => (
+    <>
+      <Navbar />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
 });
