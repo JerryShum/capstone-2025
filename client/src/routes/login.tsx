df02d7e482d139e37fc08b1806440b376c19ec70
JerryShum <jerryshum2636@gmail.com>
2025-10-10 17:40:08 -0400

    feat: create signup page

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -3,17 +3,15 @@ import { createFileRoute, Link } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { AtSign, Cat } from "lucide-react";
 import { Field, FieldLabel, FieldSeparator } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { AtSign, Cat } from "lucide-react";
 
 export const Route = createFileRoute("/login")({
   component: RouteComponent,
--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -1,9 +1,79 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/signup')({
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Field, FieldLabel, FieldSeparator } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { AtSign, Cat } from "lucide-react";
+export const Route = createFileRoute("/signup")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/signup"!</div>
+  return (
+    <div className="flex grow flex-col items-center justify-center">
+      <Card className="w-full max-w-96">
+        <CardHeader className="text-center">
+          <CardTitle className="text-primary text-2xl">Welcome!</CardTitle>
+          <CardDescription>
+            Sign up with your Github or Google account
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent className="flex flex-col gap-4">
+          <Button variant="outline" size="lg" className="w-full">
+            <Cat className="" /> Sign up with Github
+          </Button>
+          <Button variant="outline" size="lg" className="w-full">
+            <AtSign /> Sign up with Google
+          </Button>
+        </CardContent>
+
+        <div className="mx-auto w-3/4">
+          <FieldSeparator>Or continue with</FieldSeparator>
+        </div>
+
+        <CardFooter className="flex-col gap-6">
+          <Field className="gap-1">
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <Input
+              id="email"
+              placeholder="user@example.com"
+              type="email"
+              disabled
+            />
+          </Field>
+          <Field className="gap-1">
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Input
+              id="password"
+              type="password"
+              placeholder="password"
+              disabled
+            />
+          </Field>
+          <Button className="w-full">Sign up</Button>
+          <div className="flex gap-1">
+            <p className="text-muted-foreground text-sm">
+              Already have an account?
+            </p>
+
+            <Link
+              to="/login"
+              className="text-muted-foreground text-sm underline hover:brightness-75"
+            >
+              Login
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
