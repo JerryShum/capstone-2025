3f465e5d936e46f63647947b334f69f4a837102c
Jerry WSL <jerryshum2636@gmail.com>
2025-10-21 20:39:07 -0400

    feat: add get and post routes for /create/video

--- server/src/routes/create/createVideoRoute.ts
+++ server/src/routes/create/createVideoRoute.ts
@@ -1,11 +1,20 @@
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 
-export const createVideoRoute = new Hono().post('/video', async (c) => {
-   const data = {
-      message: 'hello',
-      success: true,
-   };
+export const createVideoRoute = new Hono()
+   .get('/video', async (c) => {
+      const data = {
+         message: 'hello',
+         success: true,
+      };
 
-   return c.json(data, { status: 200 });
-});
+      return c.json(data, { status: 200 });
+   })
+   .post('/video', async (c) => {
+      const data = {
+         message: 'hello',
+         success: true,
+      };
+
+      return c.json(data, { status: 200 });
+   });
