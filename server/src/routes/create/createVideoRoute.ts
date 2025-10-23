0690fb6388d6b74ee1998cfb73080f922a0b8715
Jerry WSL <jerryshum2636@gmail.com>
2025-10-23 17:19:11 -0400

    feat: create schema and server route for creatingvideo

--- server/src/routes/create/createVideoRoute.ts
+++ server/src/routes/create/createVideoRoute.ts
@@ -1,20 +1,90 @@
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import {
+   GoogleGenAI,
+   VideoGenerationReferenceImage,
+   VideoGenerationReferenceType,
+} from '@google/genai';
+
+//! Shared
+import {
+   postVideoSchema,
+   sendVideoSchema,
+} from '@shared/schemas/sendVideoSchema';
+
+//! Setting up google ai
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+if (!GEMINI_API_KEY) {
+   throw new Error('GEMINI_API_KEY is not set in the environment variables.');
+}
+
+const genAI = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
+// -----------------------------------------------------------------------------------
 
 export const createVideoRoute = new Hono()
-   .get('/video', async (c) => {
-      const data = {
-         message: 'hello',
-         success: true,
+   .post('/video', zValidator('json', postVideoSchema), async (c) => {
+      //! get validated data from zValidator
+      const postOBJ = await c.req.valid('json');
+
+      // declare interface Image_2 {
+      //     /** The Cloud Storage URI of the image. ``Image`` can contain a value
+      //      for this field or the ``image_bytes`` field but not both. */
+      //     gcsUri?: string;
+      //     /** The image bytes data. ``Image`` can contain a value for this field
+      //      or the ``gcs_uri`` field but not both.
+      //      * @remarks Encoded as base64 string. */
+      //     imageBytes?: string;
+      //     /** The MIME type of the image. */
+      //     mimeType?: string;
+      // }
+
+      const referenceImage: VideoGenerationReferenceImage = {
+         image: {
+            imageBytes: postOBJ.imageBase64,
+         },
+         referenceType: VideoGenerationReferenceType.ASSET,
       };
 
-      return c.json(data, { status: 200 });
+      let operation = await genAI.models.generateVideos({
+         model: 'veo-3.1-generate-preview',
+         prompt: postOBJ.prompt,
+         config: {
+            referenceImages: [referenceImage],
+            durationSeconds: 4,
+         },
+      });
+
+      // Poll the operation status until the video is ready.
+      while (!operation.done) {
+         console.log('Waiting for video generation to complete...');
+         await new Promise((resolve) => setTimeout(resolve, 10000));
+         operation = await genAI.operations.getVideosOperation({
+            operation: operation,
+         });
+      }
+
+      // Poll the operation status until the video is ready.
+      while (!operation.done) {
+         console.log('Waiting for video generation to complete...');
+         await new Promise((resolve) => setTimeout(resolve, 10000));
+         operation = await genAI.operations.getVideosOperation({
+            operation: operation,
+         });
+      }
+
+      // Download the video.
+      genAI.files.download({
+         file: operation.response.generatedVideos[0].video,
+         downloadPath: '../../../downloads/veo3_with_image_input.mp4',
+      });
+
+      return c.json({
+         message: 'Video generated successfully!',
+         videoUri: generatedVideo.uri,
+      });
    })
-   .post('/video', async (c) => {
-      const data = {
-         message: 'hello',
-         success: true,
-      };
-
-      return c.json(data, { status: 200 });
+   .get('/test', async (c) => {
+      return c.json({ message: 'Server is running and test route works!' });
    });
---
+++ shared/src/schemas/sendVideoSchema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const sendVideoSchema = z.object({
+   id: z.number().int().positive().min(1),
+   imageBase64: z.string(),
+   script: z.string(),
+   prompt: z.string(),
+});
+
+//@ the wanted structure of data that someone POSTS:
+export const postVideoSchema = sendVideoSchema.omit({ id: true });
