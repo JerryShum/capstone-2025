499618fa283b70df6ed5d79e662cbd98f6afb7d9
JerryShum <jerryshum2636@gmail.com>
2026-02-13 09:51:45 -0500

    setup reactflow

--- studio/src/routes/_authenticated/project/$projectID.tsx
+++ studio/src/routes/_authenticated/project/$projectID.tsx
@@ -1,16 +1,62 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useState, useCallback } from 'react';
+import {
+   ReactFlow,
+   applyNodeChanges,
+   applyEdgeChanges,
+   addEdge,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
 
 export const Route = createFileRoute('/_authenticated/project/$projectID')({
    component: RouteComponent,
 });
 
+//! THIS IS GOING TO BE THE PRIMARY PAGE FOR THE STUDIO
+// Here, we will define the entire nodebased canvas and workflow using reactflow
+// Steps:
+// 1. Retrieve the project state from the DB by searching up the project ID
+// 2. Load the project state and re-create it in the app
+// 3. User can start work
+
+//@ ---------------------------------------------------
+
+const initialNodes = [
+   { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+   { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
 function RouteComponent() {
+   const [nodes, setNodes] = useState(initialNodes);
+   const [edges, setEdges] = useState(initialEdges);
+
+   const onNodesChange = useCallback(
+      (changes) =>
+         setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+      [],
+   );
+   const onEdgesChange = useCallback(
+      (changes) =>
+         setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+      [],
+   );
+   const onConnect = useCallback(
+      (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+      [],
+   );
    const { projectID } = Route.useParams();
 
    return (
-      <div>
-         <h1>Hello "/_auth/project/$projectID"!</h1>
-         <h2>PROJECTID: {projectID}</h2>
+      <div style={{ width: '100vw', height: '100vh' }}>
+         <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            fitView
+         />
       </div>
    );
 }
