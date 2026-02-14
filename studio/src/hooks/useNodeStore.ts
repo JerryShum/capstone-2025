2e12e5c819a0a0d991dc05dc8adb074a4dde3236
JerryWSL <jerryshum2636@gmail.com>
2026-02-14 11:49:14 -0500

    define useNodeStore from reactflow

--- studio/src/hooks/useNodeStore.ts
+++ studio/src/hooks/useNodeStore.ts
@@ -1,21 +1,89 @@
 import { create } from 'zustand';
+import { addEdge, applyNodeChanges, applyEdgeChanges } from '@xyflow/react';
+import { devtools } from 'zustand/middleware';
 
-type CounterStore = {
-   nodes: {
-      id: string;
-      position: { x: number; y: number };
-      data: { label: string };
-   }[];
-   edges: { id: string; source: string; target: string }[];
-   addNode: (newNode) => void;
+//! Defining reactflow AppState
+import {
+   type Edge,
+   type Node,
+   type OnNodesChange,
+   type OnEdgesChange,
+   type OnConnect,
+} from '@xyflow/react';
+
+export type AppNode = Node;
+
+export type AppState = {
+   nodes: AppNode[];
+   edges: Edge[];
+   onNodesChange: OnNodesChange<AppNode>;
+   onEdgesChange: OnEdgesChange;
+   onConnect: OnConnect;
+   setNodes: (nodes: AppNode[]) => void;
+   setEdges: (edges: Edge[]) => void;
 };
 
-export const useNodeStore = create<CounterStore>((set) => ({
-   nodes: [
-      { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-      { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
-   ],
-   edges: [{ id: 'n1-n2', source: 'n1', target: 'n2' }],
-   //! pass in node --> set state by adding node to nodes array
-   addNode: (newNode) => set((state) => ({ nodes: [...state.nodes, newNode] })),
-}));
+const initialEdges = [
+   { id: 'e1-2', source: '1', target: '2' },
+   { id: 'e2-3', source: '2', target: '3' },
+] as Edge[];
+
+const initialNodes = [
+   {
+      id: '1',
+      type: 'input',
+      data: { label: 'Input' },
+      position: { x: 250, y: 25 },
+   },
+
+   {
+      id: '2',
+      data: { label: 'Default' },
+      position: { x: 100, y: 125 },
+   },
+   {
+      id: '3',
+      type: 'output',
+      data: { label: 'Output' },
+      position: { x: 250, y: 250 },
+   },
+] as AppNode[];
+
+// this is our useStore hook that we can use in our components to get parts of the store and call actions
+const useNodeStore = create<AppState>()(
+   devtools((set, get) => ({
+      nodes: initialNodes,
+      edges: initialEdges,
+      onNodesChange: (changes) => {
+         set({
+            nodes: applyNodeChanges(changes, get().nodes),
+         });
+      },
+      onEdgesChange: (changes) => {
+         set({
+            edges: applyEdgeChanges(changes, get().edges),
+         });
+      },
+      onConnect: (connection) => {
+         set({
+            edges: addEdge(connection, get().edges),
+         });
+      },
+      setNodes: (nodes) => {
+         set({ nodes });
+      },
+      setEdges: (edges) => {
+         set({ edges });
+      },
+   })),
+);
+
+export default useNodeStore;
+
+//# Example
+// export const useStore = create((set) => ({
+//    bears: 0,
+//$   increasePopulation: function that takes in 0 parameters --> calls the set() function --> set accesses the state --> and returns a new state where bears = state.bears + 1
+//    increasePopulation: () => set((state) => ({ bears: state.bears + 1 }))
+//    removeAllBears: () => set({ bears: 0 }),
+// }));
