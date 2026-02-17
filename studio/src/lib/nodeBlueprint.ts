d055dc67ae55e97f1247980157ad8b92e3f56d31
JerryWSL <jerryshum2636@gmail.com>
2026-02-16 20:19:14 -0500

    create a blueprint file for the custom nodes

---
+++ studio/src/lib/nodeBlueprint.ts
@@ -0,0 +1,11 @@
+//! These are the blueprints for each type of node
+// They are going to be used as the "default" values of the nodes whenever new instances are added
+
+export const nodeBlueprint = {
+   character: {
+      name: 'Character name...',
+      style: 'e.g. Cinematic, Realistic, Anime...',
+      appearance: 'e.g. Pirate, Prince, SuperHero...',
+      referenceImage: 'https://...',
+   },
+};
