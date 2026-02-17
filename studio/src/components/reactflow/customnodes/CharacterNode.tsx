7165f5e28489a1920875893b1b3eb0e247c381b6
JerryWSL <jerryshum2636@gmail.com>
2026-02-16 20:12:25 -0500

    integrate data into characterNode

--- studio/src/components/reactflow/customnodes/CharacterNode.tsx
+++ studio/src/components/reactflow/customnodes/CharacterNode.tsx
@@ -1,9 +1,11 @@
 import { Handle, Position } from '@xyflow/react';
-import type { CharacterNode } from '@/types';
+import type { CharacterNode } from '@/lib/types';
 import type { NodeProps } from '@xyflow/react';
 
 // We are getting props from NodeProps --> NodeProps is getting the "CharacterNode" props
 export default function CharacterNode({ data, id }: NodeProps<CharacterNode>) {
+   //! Data is going to be how the "state" of a node is saved
+   // data.name --> how we save the state of this node's character name
    return (
       <div className="bg-white border-2 border-slate-900 rounded-xl p-4 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] min-w-[240px] flex flex-col gap-3 font-sans">
          <Handle
@@ -21,6 +23,7 @@ export default function CharacterNode({ data, id }: NodeProps<CharacterNode>) {
             <input
                className="w-full text-sm p-2 border-2 border-slate-100 rounded-lg focus:border-blue-500 outline-none transition-colors"
                placeholder="Character name..."
+               value={data.name}
             />
          </div>
 
@@ -31,6 +34,17 @@ export default function CharacterNode({ data, id }: NodeProps<CharacterNode>) {
             <input
                className="w-full text-sm p-2 border-2 border-slate-100 rounded-lg focus:border-blue-500 outline-none transition-colors"
                placeholder="e.g. Cinematic, Anime..."
+               value={data.style}
+            />
+         </div>
+         <div className="flex flex-col gap-1">
+            <label className="text-xs font-semibold text-slate-700">
+               Appearance
+            </label>
+            <input
+               className="w-full text-sm p-2 border-2 border-slate-100 rounded-lg focus:border-blue-500 outline-none transition-colors"
+               placeholder="e.g. Cinematic, Anime..."
+               value={data.appearance}
             />
          </div>
 
@@ -41,6 +55,7 @@ export default function CharacterNode({ data, id }: NodeProps<CharacterNode>) {
             <input
                className="w-full text-sm p-2 border-2 border-slate-100 rounded-lg focus:border-blue-500 outline-none transition-colors"
                placeholder="https://..."
+               value={data.referenceImage}
             />
          </div>
 
