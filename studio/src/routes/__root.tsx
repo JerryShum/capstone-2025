a40a7868148a59615db51d40e1a6e2ff8a1b84b2
JerryWSL <jerryshum2636@gmail.com>
2026-02-18 11:16:18 -0500

    add shadcn tooltip provider

--- studio/src/routes/__root.tsx
+++ studio/src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 export const Route = createRootRoute({
    component: RootComponent,
@@ -8,7 +9,9 @@ export const Route = createRootRoute({
 function RootComponent() {
    return (
       <React.Fragment>
-         <Outlet />
+         <TooltipProvider>
+            <Outlet />
+         </TooltipProvider>
       </React.Fragment>
    );
 }
