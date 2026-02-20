9a66e7d41e362236c6e7fd0b56b83cef1b963d4b
JerryWSL <jerryshum2636@gmail.com>
2026-02-20 12:18:54 -0500

    convert updateScriptNode --> updateNode to be used as a general function

--- studio/src/hooks/useFlowStore.ts
+++ studio/src/hooks/useFlowStore.ts
@@ -2,7 +2,12 @@ import { create } from 'zustand';
 import { addEdge, applyNodeChanges, applyEdgeChanges } from '@xyflow/react';
 import type { Edge } from '@xyflow/react';
 import { devtools } from 'zustand/middleware';
-import type { FlowState, AppNode } from '@/lib/types';
+import type {
+   FlowState,
+   AppNode,
+   ScriptNode,
+   CharacterNode,
+} from '@/lib/types';
 import { nodeBlueprint } from '@/lib/nodeBlueprint';
 
 const initialEdges = [
@@ -10,29 +15,29 @@ const initialEdges = [
    { id: 'e2-3', source: '2', target: '3' },
 ] as Edge[];
 
-const initialNodes = [
-   {
-      id: '1',
+const initCharacterNode: CharacterNode = {
+   id: '1',
+   type: 'character',
+   data: {
       type: 'character',
-      data: {
-         type: 'character',
-         name: 'barry',
-         style: 'pixelated cartoon chicken',
-         appearance: 'pixelated chicken, yellow hair, big blue eyes',
-      },
-      position: { x: 250, y: 25 },
+      name: 'barry',
+      style: 'pixelated cartoon chicken',
+      appearance: 'pixelated chicken, yellow hair, big blue eyes',
    },
-   {
-      id: '2',
+   position: { x: 250, y: 25 },
+};
+
+const initScriptNode: ScriptNode = {
+   id: '2',
+   type: 'script',
+   data: {
       type: 'script',
-      data: {
-         type: 'script',
-         name: 'barry',
-         visuals: 'pixelated chicken, yellow hair, big blue eyes',
-      },
-      position: { x: 250, y: 25 },
+      content: 'script for barry the chicken:yapyapyapypaypaypyapp',
    },
-] as AppNode[];
+   position: { x: 250, y: 25 },
+};
+
+const initialNodes = [initCharacterNode, initScriptNode] as AppNode[];
 
 // this is our useStore hook that we can use in our components to get parts of the store and call actions
 const useFlowStore = create<FlowState>()(
@@ -79,21 +84,21 @@ const useFlowStore = create<FlowState>()(
          // set the new state to include newNode
          set({ nodes: [...get().nodes, newNode] });
       },
-      updateScriptNode: (id, data) => {
+      updateNode: (id, data) => {
          // filter out all the nodes and find the one we need to update using id
          set({
             nodes: get().nodes.map((node) => {
                if (node.id === id) {
                   // we found the node we need to update --> create new node with the new data
 
-                  const newScriptNode = {
+                  const newNode = {
                      // spread the general "node" properties
                      ...node,
                      // overwriting the old data with new data (keeps whatever wasn't updated)
                      data: { ...node.data, ...data },
                   } as AppNode;
 
-                  return newScriptNode;
+                  return newNode;
                }
 
                return node;
