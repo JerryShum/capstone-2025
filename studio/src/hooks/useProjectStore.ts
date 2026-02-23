646028a9b27ab9bf36231a008367a4c6d16c0e2b
JerryShum <jerryshum2636@gmail.com>
2026-02-23 13:22:32 -0500

    create new store for projectSettings & name

---
+++ studio/src/hooks/useProjectStore.ts
@@ -0,0 +1,12 @@
+import type { ProjectState } from '@/lib/projectTypes';
+import { create } from 'zustand';
+
+export const useProjectStore = create<ProjectState>((set, get) => ({
+   projectTitle: 'default',
+   projectSettings: {
+      aspectRatio: '16:9',
+      engine: 'veo',
+      globalNegativePrompt: 'something here',
+      executiveSummary: 'summary of your story goes here...',
+   },
+}));
