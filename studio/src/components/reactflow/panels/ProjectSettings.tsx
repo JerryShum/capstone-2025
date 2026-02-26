06ed14d6757db7baa98a657c06ab6c4418aba14d
JerryWSL <jerryshum2636@gmail.com>
2026-02-26 14:48:56 -0500

    adjust badge border colour

--- studio/src/components/reactflow/panels/ProjectSettings.tsx
+++ studio/src/components/reactflow/panels/ProjectSettings.tsx
@@ -24,12 +24,12 @@ export default function ProjectSettings({
       <Sheet open={openState} onOpenChange={setProjectOpen}>
          <SheetContent
             side="left"
-            className="w-full sm:max-w-[440px]  backdrop-blur-xl border-l border-white/10 shadow-2xl p-0 flex flex-col gap-0 overflow-hidden rounded-r-lg"
+            className="w-full sm:max-w-[440px] backdrop-blur-xl border-l border-white/10 shadow-2xl p-0 flex flex-col gap-0 overflow-hidden rounded-r-lg"
          >
             <div className="flex-1 overflow-y-auto px-6 py-8 custom-scrollbar">
                <SheetHeader className="mb-8 px-0">
                   <div className="flex items-center justify-between mb-2">
-                     <div className="flex items-center gap-2.5 py-1 bg-white/10 border border-foreground/10 rounded-full w-fit px-3">
+                     <div className="flex items-center gap-2.5 py-1 bg-white/10  border-2 border-foreground/15 rounded-full w-fit px-3">
                         <Settings2 size={16} className="text-foreground/70" />
                         <span className="text-[11px] font-black uppercase tracking-[0.1em] text-foreground/70">
                            Global DNA
