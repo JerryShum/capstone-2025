fdbed877adafecd24da1479ef67a163e0351f8d9
JerryWSL <jerryshum2636@gmail.com>
2026-03-01 17:35:24 -0500

    move reactflow defaults/initals to shared directory

---
+++ shared/src/types/reactflow/defaultProject.ts
@@ -0,0 +1,55 @@
+import type {
+   AppNode,
+   CharacterNode,
+   ProjectSettingsNode,
+   SceneNode,
+   ScriptNode,
+} from './flowTypes';
+
+import type { Edge } from '@xyflow/react';
+
+const initCharacterNode: CharacterNode = {
+   id: '1-defaultchar',
+   type: 'character',
+   data: {
+      type: 'character',
+      name: 'barry',
+      style: 'pixelated cartoon chicken',
+      appearance: 'pixelated chicken, yellow hair, big blue eyes',
+   },
+   position: { x: 500, y: 25 },
+};
+
+const initScriptNode: ScriptNode = {
+   id: '2-initscript',
+   type: 'script',
+   data: {
+      type: 'script',
+      content: 'script for barry the chicken:yapyapyapypaypaypyapp',
+   },
+   position: { x: 250, y: 25 },
+};
+
+const initSceneNode: SceneNode = {
+   id: '3-initscene',
+   type: 'scene',
+   data: {
+      type: 'scene',
+      scenePrompt: 'Barry the chicken is going about his day around the barn.',
+      duration: 8,
+      shotType: 'Close-up',
+      cameraMovement: 'Dolly',
+      status: 'IDLE',
+      videoURL: 'https://',
+      thumbnailURL: 'https://',
+   },
+   position: { x: 0, y: 25 },
+};
+
+export const initialNodes = [
+   initCharacterNode,
+   initScriptNode,
+   initSceneNode,
+] as AppNode[];
+
+export const initialEdges = [] as Edge[];
