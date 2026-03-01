1680583ae3ef26b6f1f7f2cdbcee9bc640a0a765
JerryWSL <jerryshum2636@gmail.com>
2026-03-01 15:53:00 -0500

    incorporate maxSaveTime into the debounce function

--- studio/src/lib/functions/debounce.ts
+++ studio/src/lib/functions/debounce.ts
@@ -6,22 +6,45 @@
 
 //# This is called a higher order (function that takes in another function or return one) and a CLOSURE --> functions that have a state
 //  closures remember a value that persists through executions / calls
+
+//@ Defining the maxWait / throttle functionality
+// when the function is called --> check if there
 export default function debounce<
    ArgsType extends any[],
    FuncType extends (...args: ArgsType) => void,
->(func: FuncType, delay: number): (...args: Parameters<FuncType>) => void {
+>(
+   func: FuncType,
+   delay: number,
+   maxWaitTime: number,
+): (...args: Parameters<FuncType>) => void {
    //the persistent variable / state of the debounce function
    let timeoutID: ReturnType<typeof setTimeout> | undefined;
+   let lastSaveTime: number = 0;
 
    // the function that will actually be called
    return (...args: Parameters<FuncType>) => {
+      const now = Date.now();
+
+      // set the beginning of the save timer (if there isn't one already)
+      if (!lastSaveTime) {
+         lastSaveTime = now;
+      }
+
       //if the debounce function is called with a pre-existing timeout --> kill it --> we set a new one after!
       if (timeoutID) {
          clearTimeout(timeoutID);
       }
 
-      timeoutID = setTimeout(() => {
+      // check if its been 10 / 30 seconds since the "lastSaveTime" --> if yes, call the save function --> reset the saveTime
+      if (now - lastSaveTime >= maxWaitTime) {
          func(...args);
-      }, delay);
+         lastSaveTime = now;
+      } else {
+         timeoutID = setTimeout(() => {
+            func(...args);
+
+            lastSaveTime = 0;
+         }, delay);
+      }
    };
 }
