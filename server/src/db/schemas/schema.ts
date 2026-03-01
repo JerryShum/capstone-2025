d9d95befa38d7469af8241154a3de2c8e7acdfa8
JerryWSL <jerryshum2636@gmail.com>
2026-03-01 16:24:05 -0500

    create database schema for the projectsTable

--- server/src/db/schemas/schema.ts
+++ server/src/db/schemas/schema.ts
@@ -1,8 +1,21 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import {
+   integer,
+   jsonb,
+   pgTable,
+   varchar,
+   timestamp,
+} from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users', {
    id: integer().primaryKey().generatedAlwaysAsIdentity(),
    name: varchar({ length: 255 }).notNull(),
    age: integer().notNull(),
    email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const projectsTable = pgTable('projects', {
+   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+   projectTitle: varchar({ length: 255 }).notNull(),
+   flowData: jsonb('flow_data').notNull(),
+   updatedAt: timestamp('updated_at').defaultNow(),
 });
