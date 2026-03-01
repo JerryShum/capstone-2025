76eae9b5ebed308b2c2563250fe1d057ec1b9589
JerryShum <jerryshum2636@gmail.com>
2026-03-01 11:20:57 -0500

    implement subscribeWithSelector to isolate for nodes and edges state

--- studio/src/hooks/useFlowStore.ts
+++ studio/src/hooks/useFlowStore.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { addEdge, applyNodeChanges, applyEdgeChanges } from '@xyflow/react';
 import type { Edge } from '@xyflow/react';
-import { devtools } from 'zustand/middleware';
+import { devtools, subscribeWithSelector } from 'zustand/middleware';
 import type {
    FlowState,
    AppNode,
@@ -62,103 +62,105 @@ const initialNodes = [
 
 // this is our useStore hook that we can use in our components to get parts of the store and call actions
 const useFlowStore = create<FlowState>()(
-   devtools((set, get) => ({
-      nodes: initialNodes,
-      edges: initialEdges,
-      onNodesChange: (changes) => {
-         set({
-            nodes: applyNodeChanges(changes, get().nodes),
-         });
-      },
-      onEdgesChange: (changes) => {
-         set({
-            edges: applyEdgeChanges(changes, get().edges),
-         });
-      },
-      onConnect: (connection) => {
-         set({
-            edges: addEdge(connection, get().edges),
-         });
-      },
-      setNodes: (nodes) => {
-         set({ nodes });
-      },
-      setEdges: (edges) => {
-         set({ edges });
-      },
-      addNode: (type, position) => {
-         console.log('addNode');
-         // use the blueprint to create a "default" node based on the type
-         // character | scene | projectSettings | script
-         const blueprint = nodeBlueprint[type];
-         const { x, y } = position;
+   subscribeWithSelector(
+      devtools((set, get) => ({
+         nodes: initialNodes,
+         edges: initialEdges,
+         onNodesChange: (changes) => {
+            set({
+               nodes: applyNodeChanges(changes, get().nodes),
+            });
+         },
+         onEdgesChange: (changes) => {
+            set({
+               edges: applyEdgeChanges(changes, get().edges),
+            });
+         },
+         onConnect: (connection) => {
+            set({
+               edges: addEdge(connection, get().edges),
+            });
+         },
+         setNodes: (nodes) => {
+            set({ nodes });
+         },
+         setEdges: (edges) => {
+            set({ edges });
+         },
+         addNode: (type, position) => {
+            console.log('addNode');
+            // use the blueprint to create a "default" node based on the type
+            // character | scene | projectSettings | script
+            const blueprint = nodeBlueprint[type];
+            const { x, y } = position;
 
-         // define new node structure
-         const newNode: AppNode = {
-            id: (get().nodes.length + 1).toString() + '-' + Date.now(),
-            type: type,
-            //$ Position should be changed to fit the middle of current viewport
-            position: { x, y },
-            data: blueprint.defaultData,
-         };
+            // define new node structure
+            const newNode: AppNode = {
+               id: (get().nodes.length + 1).toString() + '-' + Date.now(),
+               type: type,
+               //$ Position should be changed to fit the middle of current viewport
+               position: { x, y },
+               data: blueprint.defaultData,
+            };
 
-         // set the new state to include newNode
-         set({ nodes: [...get().nodes, newNode] });
-      },
-      updateNode: (id, data) => {
-         // filter out all the nodes and find the one we need to update using id
-         set({
-            nodes: get().nodes.map((node) => {
-               if (node.id === id) {
-                  // we found the node we need to update --> create new node with the new data
+            // set the new state to include newNode
+            set({ nodes: [...get().nodes, newNode] });
+         },
+         updateNode: (id, data) => {
+            // filter out all the nodes and find the one we need to update using id
+            set({
+               nodes: get().nodes.map((node) => {
+                  if (node.id === id) {
+                     // we found the node we need to update --> create new node with the new data
 
-                  const newNode = {
-                     // spread the general "node" properties
-                     ...node,
-                     // overwriting the old data with new data (keeps whatever wasn't updated)
-                     data: { ...node.data, ...data },
-                  } as AppNode;
+                     const newNode = {
+                        // spread the general "node" properties
+                        ...node,
+                        // overwriting the old data with new data (keeps whatever wasn't updated)
+                        data: { ...node.data, ...data },
+                     } as AppNode;
 
-                  return newNode;
-               }
+                     return newNode;
+                  }
 
-               return node;
-            }),
-         });
-      },
-      deleteNode: (id) => {
-         set({
-            //@ Only kepe the nodes with IDS that DO NOT match the input
-            nodes: get().nodes.filter((node) => !(node.id == id)),
-            edges: get().edges.filter(
-               //@ Only keeping the edges where their source AND target DONT match the input
-               (edge) => !(edge.source == id) && !(edge.target == id),
-            ),
-         });
-      },
-      duplicateNode: (id) => {
-         const nodesArray = get().nodes;
+                  return node;
+               }),
+            });
+         },
+         deleteNode: (id) => {
+            set({
+               //@ Only kepe the nodes with IDS that DO NOT match the input
+               nodes: get().nodes.filter((node) => !(node.id == id)),
+               edges: get().edges.filter(
+                  //@ Only keeping the edges where their source AND target DONT match the input
+                  (edge) => !(edge.source == id) && !(edge.target == id),
+               ),
+            });
+         },
+         duplicateNode: (id) => {
+            const nodesArray = get().nodes;
 
-         const originalNode = nodesArray.find((node) => {
-            return node.id == id;
-         });
+            const originalNode = nodesArray.find((node) => {
+               return node.id == id;
+            });
 
-         if (!originalNode) return;
+            if (!originalNode) return;
 
-         const duplicate = {
-            ...originalNode,
-            id: nodesArray.length.toString() + '-' + Date.now(),
-            position: {
-               x: originalNode.position.x + 200,
-               y: originalNode.position.y + 200,
-            },
-         } as AppNode;
+            const duplicate = {
+               ...originalNode,
+               id: nodesArray.length.toString() + '-' + Date.now(),
+               position: {
+                  x: originalNode.position.x + 200,
+                  y: originalNode.position.y + 200,
+               },
+            } as AppNode;
 
-         set({
-            nodes: [...nodesArray, duplicate],
-         });
-      },
-   })),
+            set({
+               nodes: [...nodesArray, duplicate],
+            });
+         },
+      })),
+   ),
 );
 
 export default useFlowStore;
--- studio/src/lib/persistence.ts
+++ studio/src/lib/persistence.ts
@@ -1,12 +1,23 @@
 import useFlowStore from '@/hooks/useFlowStore';
 import { useProjectStore } from '@/hooks/useProjectStore';
 
-export const unsub = useFlowStore.subscribe((state, prevState) => {
-   console.log(state, prevState);
-});
+//! Creation of store subscribers --> these subscribe to any updates to their store
+// the function runs whenever the store state is updated
+export const unsub = useFlowStore.subscribe(
+   (state) => [state.nodes, state.edges],
+   (state, prevState) => {
+      const nodeState = state[0];
+      const edgeState = state[1];
+      console.log('Nodes:', nodeState);
+      console.log('Edges', edgeState);
+      // call debounce function here --> sends the state to the server (after an amount of time after an action occurred)
+   },
+);
 
 export const unsub2 = useProjectStore.subscribe((state, prevState) => {
    console.log(state, prevState);
+
+   console.log(state.projectTitle);
 });
 
 // unsub();
