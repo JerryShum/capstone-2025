480143e9132e697afdfc6fba7c135ea28e603fd7
jonathanhua <jonathanhua@hotmail.com>
2026-03-02 01:21:46 -0500

    added account management and login endpoints

---
+++ server/src/routes/account/loginRoute.ts
@@ -0,0 +1,42 @@
+import {Hono} from 'hono';
+import { zValidator } from '@hono/zod-validator';
+import { loginSchema } from '@shared/schemas/loginSchema';
+import { usersTable } from '@server/db/schemas/schema';
+import { db } from '../../db'; 
+import { eq } from 'drizzle-orm';
+
+
+export const loginRoute = new Hono()
+    .post('/login', zValidator('json',loginSchema), async(c) =>{
+        const info = await c.req.valid('json');
+
+        // finding if user exists in database
+        const result = await db
+            .select()
+            .from(usersTable)
+            .where(eq(usersTable.email, info.email))
+        
+        const user = result[0];
+        if (!user) {
+            return c.json({ message: "Invalid credentials" }, 401);
+        }
+
+
+        // verify if pass is correct
+        if (!(info.password === user.password)){
+            return c.json({message: "Invalid username/password"}, 401);
+        }
+        else{
+            // return success msg
+            return c.json({
+                success: true,
+                message: "logged in success!",
+                user:{
+                    id: user.id,
+                    name: user.name,
+                    email: user.email
+                }
+            }, 200)
+        }
+    })
+    
