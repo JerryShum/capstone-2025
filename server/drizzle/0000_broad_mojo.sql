730bef34f04b9b531bccebc3e7f6aacd717865c2
JerryWSL <jerryshum2636@gmail.com>
2026-03-01 19:21:11 -0500

    push changes to the drizzle db

---
+++ server/drizzle/0000_broad_mojo.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "projects" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "projects_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"projectTitle" varchar(255) NOT NULL,
+	"flow_data" jsonb NOT NULL,
+	"updated_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(255) NOT NULL,
+	"age" integer NOT NULL,
+	"email" varchar(255) NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
