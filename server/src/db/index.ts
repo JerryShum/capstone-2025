e87d779a807f21871d2f21537c73867b5713eb74
jonathanhua <jonathanhua@hotmail.com>
2026-03-02 01:19:11 -0500

    setup connection to postgresql database

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,3 +1,12 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres'
+import * as schema from './schemas/schema'
 
-export const db = drizzle(process.env.DATABASE_URL!);
+// stup connection link
+export const db_conn = process.env.DATABASE_URL!;
+
+// client (driver)
+const client = postgres(db_conn, {prepare: false});
+
+// the database (drizzle instance)
+export const db = drizzle(client, {schema})
