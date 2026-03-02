e25f5806afbe5a897aada6fb8f67025e18f408a6
jonathanhua <jonathanhua@hotmail.com>
2026-03-02 15:53:38 -0500

    added login api test

--- server/src/index.test.ts
+++ server/src/index.test.ts
@@ -1,7 +1,20 @@
 import { expect, test, describe } from "bun:test";
+import { loginRoute } from "./routes/account/loginRoute";
 
 describe("testing", () => {
   test("should pass to verify CI working", () => {
     expect(1 + 1).toBe(2);
   });
+});
+
+describe("Login API", ()=>{
+    test("POST /login: empty body, return 400", async() =>{
+        const res = await loginRoute.request("/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({}),
+        });
+
+        expect(res.status).toBe(400);
+    });
 });
