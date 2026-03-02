e139c50c65469e244b70c1aaba9f4ee01c3e15c8
jonathanhua <jonathanhua@hotmail.com>
2026-03-02 16:08:09 -0500

    added valid login test

--- server/src/index.test.ts
+++ server/src/index.test.ts
@@ -1,13 +1,16 @@
 import { expect, test, describe } from "bun:test";
 import { loginRoute } from "./routes/account/loginRoute";
 
+// testing test function
 describe("testing", () => {
   test("should pass to verify CI working", () => {
     expect(1 + 1).toBe(2);
   });
 });
 
-describe("Login API", ()=>{
+describe("Login API endpoint", ()=>{
+
+    // test empty login
     test("POST /login: empty body, return 400", async() =>{
         const res = await loginRoute.request("/login", {
             method: "POST",
@@ -17,4 +20,21 @@ describe("Login API", ()=>{
 
         expect(res.status).toBe(400);
     });
+
+
+    // test valid login
+    test("POST /login: valid login, return 200", async() =>{
+        const res = await loginRoute.request("/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                "email": "test@example.com",
+                "password": "password123"
+            }),
+        });
+
+        expect(res.status).toBe(200);
+    });
+
 });
+
