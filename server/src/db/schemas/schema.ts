fd6a4a0d6cc916a724dd4848286d10f21ae10bb3
jonathanhua <jonathanhua@hotmail.com>
2026-03-02 01:20:01 -0500

    added shared login schema

--- server/src/db/schemas/schema.ts
+++ server/src/db/schemas/schema.ts
@@ -11,6 +11,7 @@ export const usersTable = pgTable('users', {
    name: varchar({ length: 255 }).notNull(),
    age: integer().notNull(),
    email: varchar({ length: 255 }).notNull().unique(),
+   password: varchar({length: 255}).notNull(),
 });
 
 export const projectsTable = pgTable('projects', {
