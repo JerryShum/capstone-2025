81197682a4be90d8e64bdd5cf592a0095baa3010
JerryWSL <jerryshum2636@gmail.com>
2026-03-05 17:56:23 -0500

    server: setting up video generation and status routes.

--- server/src/routes/studio/videoRoute.ts
+++ server/src/routes/studio/videoRoute.ts
@@ -1,20 +1,79 @@
 import { zValidator } from '@hono/zod-validator';
 import { postVideoSchema } from '@shared/schemas/sendVideoSchema';
 import { Hono } from 'hono';
+import { buildGCPVideoPrompt } from '@server/functions/studio/buildGCPVideoPrompt';
+import { GenerateVideosOperation, GoogleGenAI } from '@google/genai';
 
-export const videoRoute = new Hono().post(
-   '/generate',
-   zValidator('json', postVideoSchema),
-   async (c) => {
+//---------------------------------------------------------
+
+//! Google Gemini API Setup
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+if (!GEMINI_API_KEY) {
+   throw new Error('GEMINI_API_KEY is not set in the environment variables.');
+}
+
+const genAI = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
+export const videoRoute = new Hono()
+   .post('/generate', zValidator('json', postVideoSchema), async (c) => {
       // get validated data from validator
       const data = c.req.valid('json');
 
       //---------------------------------------------------------
-      console.log('FACTORY SERVER RECEIVED SCENENODE DATA:', data);
+      // create master prompt using prompt builder function:
+      const masterPrompt = buildGCPVideoPrompt(data);
+      console.log('--- MASTER PROMPT BUILT ---');
+      console.log(masterPrompt);
+      console.log('---------------------------');
 
-      return c.json({
-         operationName: 'mock-ticket-' + Date.now(),
-         message: 'Factory is ready for work!',
+      //---------------------------------------------------------
+      //@ Gemini api call
+      try {
+         const operation = await genAI.models.generateVideos({
+            model: 'veo-3.1-fast-generate-preview',
+            prompt: masterPrompt,
+            config: {
+               durationSeconds: 8,
+            },
+         });
+
+         if (!operation) {
+            throw new Error('SERVER: Failed to start video generation.');
+         }
+
+         return c.json({
+            operationName: operation.name,
+         });
+      } catch (error) {
+         return c.json(
+            {
+               message: error,
+            },
+            500,
+         );
+      }
+   })
+   .get('/status/:name{.+}', async (c) => {
+      // get the operationName from the URL
+      const operationName = c.req.param('name');
+
+      // ask gemini for the status on the operation
+      const operationBuild = new GenerateVideosOperation();
+      operationBuild.name = operationName;
+
+      const operation = await genAI.operations.getVideosOperation({
+         operation: operationBuild,
       });
-   },
-);
+
+      //@ return the state of the operation
+      // if the operation is not done yet
+      if (!operation.done) {
+         return c.json({ status: 'PROCESSING', videosURL: undefined });
+      }
+
+      if (operation.error) {
+         return c.json({ status: 'ERROR', videosURL: undefined });
+      }
+
+      return c.json({ status: 'READY_TO_DOWNLOAD' });
+   });
