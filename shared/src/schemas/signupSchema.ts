a65f397ec7bb1307fbd6db79ae11f0d1fb02a94b
JerryWSL <jerryshum2636@gmail.com>
2026-03-24 14:16:48 -0400

    client/shared: updating signupSchema to account for proper email validation.

--- shared/src/schemas/signupSchema.ts
+++ shared/src/schemas/signupSchema.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 
 export const signupSchema = z.object({
-    name: z.string().min(3, "Name is required. Please enter your name."),
-    email: z.string({ message: "Invalid email format. Please enter a valid email address." }),
-    password: z.string().min(8, "Password must be atleast 8 characters long.")
-})
+   name: z.string().min(3, 'Name is required. Please enter your name.'),
+   email: z.email({
+      message: 'Invalid email format. Please enter a valid email address.',
+   }),
+   password: z.string().min(8, 'Password must be atleast 8 characters long.'),
+});
 
-export type SignupInput = z.infer<typeof signupSchema>
+export type SignupInput = z.infer<typeof signupSchema>;
