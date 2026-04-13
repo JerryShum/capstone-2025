3a0048885319bcf8d33c9202401e6edde951b03c
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2026-04-12 23:22:08 -0400

    Revert "feat: added copy-to-clipboard share button for stitched videos"

--- studio/src/routes/__root.tsx
+++ studio/src/routes/__root.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from '@/components/ui/sonner';
 
 export const Route = createRootRoute({
    component: RootComponent,
@@ -16,7 +15,6 @@ function RootComponent() {
          <QueryClientProvider client={queryClient}>
             <TooltipProvider>
                <Outlet />
-               <Toaster position="bottom-right" richColors />
             </TooltipProvider>
          </QueryClientProvider>
       </React.Fragment>
